be0f595ad7d8f6757fd8663906da29e8ec8ee998
[diego] <[dielmac7@gmail.com]>
2022-07-28 15:53:35 -0300

    sass added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import styles from './../scss/test.module.scss'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <div>Welcome to Timy !!!</div>
+        <Link to="/pomodoro" className={styles.test} >Go to pomodoro</Link>
     )
 }
