055a03cc9a3eb9e21102461f5df90e431adfb638
[diego] <[dielmac7@gmail.com]>
2022-08-01 16:31:25 -0300

    adding nav styles

--- src/components/pomodoro/Navigation.js
+++ src/components/pomodoro/Navigation.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { AdjustmentIcon } from '../svgs/AdjustmentIcon'
+import { ClockIcon } from '../svgs/ClockIcon'
+import { HomeIcon } from '../svgs/HomeIcon'
+
+import styles from '../../scss/components/pomodoro.module.scss'
+import { Link } from 'react-router-dom'
 
 export const Navigation = () => {
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
-                <li>go back</li>
-                <li>clock</li>
-                <li>Settings</li>
+                <li>
+                    <HomeIcon />
+                    <Link to='/'>Home</Link>
+
+                </li>
+                <li>
+                    <ClockIcon />
+                    <Link to="/pomodoro">Pomodoro</Link>
+
+
+                </li>
+                <li>
+                    <AdjustmentIcon />
+                    <Link to="/">Settings</Link>
+
+
+                </li>
             </ul>
         </nav>
     )
---
+++ src/components/svgs/ClockIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ClockIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-clock" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="12" r="9" />
+            <polyline points="12 7 12 12 15 15" />
+        </svg>
+    )
+}
